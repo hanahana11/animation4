0e1be264e0bd1df508f97788c806e7e4af8f15fe
hanahana11 <hana87kotaro@i.softbank.jp>
2019-01-04 22:56:44 +0900

    Initial Commit

---
+++ animation4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animation4
+//
+//  Created by 末利　花 on 2019/01/04.
+//  Copyright © 2019年 末利　花. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
